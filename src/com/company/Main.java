f8d2537a3d6d200e77b205c3210f8caffbf379c5
Angel <86420291+malelelonely@users.noreply.github.com>
2021-12-01 17:07:18 +0200

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,33 @@
 package com.company;
-
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Random random = new Random();
         Battle pokemon1 = new Battle("Pikachu",100,50,50,"mouse","electric","small");
-        System.out.println(pokemon1);
         Battle pokemon2 = new Battle("Baltazar",200,30,70,"turtle","water","medium");
-        System.out.println(pokemon2);
         Battle pokemon3 = new Battle("Balrock",90,60,45,"bull","earthquake","large");
-        System.out.println(pokemon3);
         Battle pokemon4 = new Battle("Rintar",140,110,70,"butterfly","air","medium");
-        System.out.println(pokemon4);
         Battle pokemon5 = new Battle("Valon",95,85,130,"dragon","fire","large");
-        System.out.println(pokemon5);
-      //  System.out.println();
-        pokemon1.battle(pokemon1,pokemon2);
+        System.out.println("Choose pokemon :");
+        System.out.println("1. Pikachu");
+        System.out.println("2. Baltazar");
+        System.out.println("3. Balrock");
+        System.out.println("4. Rintar");
+        System.out.println("5. Valon");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Your choose : ");
+        int n = in.nextInt();
+        //pokemon1.battle(pokemon1,pokemon2);
+        switch(n){
+            case 1 :
+                System.out.println("You have chosen Pikachu");
+                pokemon1.battle(pokemon1,pokemon2);
 
+        }
         
 
     }
+
 }
