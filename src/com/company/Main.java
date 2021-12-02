1ce59df2016ed5a6c259fdf75d5ef275808c3469
Angel <86420291+malelelonely@users.noreply.github.com>
2021-12-02 18:04:14 +0200

    project v3.00

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,11 @@ public class Main {
         Random random = new Random();
         Scanner input = new Scanner(System.in);
         Battle[] pokemon = new Battle[5];
-        pokemon[0] = new Battle("Pikachu",100,50,50,"mouse","electric","small");
-        pokemon[1] = new Battle("Baltazar",200,30,70,"turtle","water","medium");
-        pokemon[2] = new Battle("Balrock",90,60,45,"bull","earthquake","large");
-        pokemon[3] = new Battle("Rintar",140,110,70,"butterfly","air","medium");
-        pokemon[4] = new Battle("Valon",95,85,130,"dragon","fire","large");
+        pokemon[0] = new Battle("Pikachu", 310, 50, 50, "mouse", "electric", "small");
+        pokemon[1] = new Battle("Baltazar", 340, 70, 70, "turtle", "water", "medium");
+        pokemon[2] = new Battle("Balrock", 300, 60, 45, "bull", "earthquake", "large");
+        pokemon[3] = new Battle("Rintar", 250, 110, 70, "butterfly", "air", "medium");
+        pokemon[4] = new Battle("Valon", 285, 85, 130, "dragon", "fire", "large");
         System.out.println("Choose pokemon :");
         System.out.println("1. Pikachu");
         System.out.println("2. Baltazar");
@@ -22,32 +22,29 @@ public class Main {
         System.out.print("Your choose : ");
         int n = in.nextInt();
 
-        switch(n){
-            case 1 :
+        switch (n) {
+            case 1:
                 System.out.println("You have chosen Pikachu");
-                pokemon[0].battle(pokemon[0],pokemon[random.nextInt(4)]);
-                int restart = input.nextInt();
-                if(restart == 1){
-                pokemon[0].battle(pokemon[0],pokemon[random.nextInt(4)]);
-                }else if(restart == 2){
-                    System.out.println("Thank you for playing!");
-                }
-                ;break;
+                pokemon[0].battle(pokemon[0], pokemon[random.nextInt(4)]);
+                break;
+
             case 2:
                 System.out.println("You have chosen Baltazar");
-                pokemon[1].battle(pokemon[1],pokemon[random.nextInt(4)]);break;
+                pokemon[1].battle(pokemon[1], pokemon[random.nextInt(4)]);
+                break;
             case 3:
                 System.out.println("You have chosen Balrock");
-                pokemon[2].battle(pokemon[2],pokemon[random.nextInt(4)]);break;
+                pokemon[2].battle(pokemon[2], pokemon[random.nextInt(4)]);
+                break;
             case 4:
                 System.out.println("You have chosen Rintar");
-                pokemon[3].battle(pokemon[3],pokemon[random.nextInt(4)]);break;
+                pokemon[3].battle(pokemon[3], pokemon[random.nextInt(4)]);
+                break;
             case 5:
                 System.out.println("You have chosen Valon");
-                pokemon[4].battle(pokemon[4],pokemon[random.nextInt(4)]);break;
+                pokemon[4].battle(pokemon[4], pokemon[random.nextInt(4)]);
+                break;
         }
-        
 
     }
-
 }
